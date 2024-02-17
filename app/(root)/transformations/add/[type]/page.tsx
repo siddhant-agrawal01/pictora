f974c6b50daa8dd91810ac32c718d80aed889f96
Siddhant Agrawal <siddhant_010@outlook.com>
2024-02-17 23:31:27 +0530

    first commit

---
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AddTransformationsTypePage = () => {
+  return <div>AddTransformations</div>;
+};
+
+export default AddTransformationsTypePage;
---
+++ app/layout.tsx
@@ -0,0 +1,32 @@
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import "./globals.css";
+import { cn } from "@/lib/utils";
+import { ClerkProvider } from "@clerk/nextjs";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  variable: "--font-poppins",
+});
+
+export const metadata: Metadata = {
+  title: "Pictora",
+  description: "AI powered image generator",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ClerkProvider appearance={{ variables: { colorPrimary: "#48D4CA" } }}>
+      <html lang="en">
+        <body className={cn("font-poppins antialiased", poppins.variable)}>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
